7564b3019251d05c4eb5b46204b2eeecefa5281a
Sabina Ceja-Siete <sabinasiete@gmail.com>
2022-11-18 02:52:26 -0800

    added api

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var apiKey = "31e9d74d1969cc655cd5d62c1919b31d";
+var lat = 47.6062
+var lon = 122.3321
+
+var endPoint =`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+// console.log(endPoint)
+
+fetch(endPoint)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
