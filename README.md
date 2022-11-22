000a6d53a63aad10b10bfcc8030adef650e48286
Sabina Ceja-Siete <sabinasiete@gmail.com>
2022-11-21 21:41:41 -0800

    fetch api is working now

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Weather-Dash
+
+## Description
+Using a third party API, Javascript, HTML and CSS. To fetch data to recieve current weather data while using local storage to store data.
+
+## Resources 
+https://openweathermap.org/forecast5
+
+## Contact Me
+sabinasiete@gmail.com
+
+## Screenshot
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var apiKey = "31e9d74d1969cc655cd5d62c1919b31d";
 var lat = 47.6062
 var lon = 122.3321
 
-var endPoint =`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+var endPoint =`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
 // console.log(endPoint)
 
