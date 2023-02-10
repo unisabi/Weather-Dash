d92db5b3cb5f0d97b3388df6d2ad10904257e9cf
Sabina Ceja-Siete <sabinasiete@gmail.com>
2023-02-10 09:57:07 -0800

    put in ss and finished up readme

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # Weather-Dash
 
-## Description
-Using a third party API, Javascript, HTML and CSS. To fetch data to recieve current weather data while using local storage to store data.
+## Screenshot
+![image](./assets/images/weather.png)
 
+## Usage
+Upon loading the landing page, you will see on the top left an option to seach for a city, when you type in any city name and click on the search button. This will populate current weather on the top right showing the Temp, Wind and Humidity with a lovely image of todays forcast. Below, on the bottom right will give you a five day advance of the weather forcast. This will save any previous search city history, so you can click on them to look back at the city's weather. 
+## Built With
+ * Api
+ * Javascript
+ * HTML
+ * CSS
+ * Local Storage
 ## Resources 
 https://openweathermap.org/forecast5
 
 ## Contact Me
-sabinasiete@gmail.com
+* [Sabina Ceja-Siete](https://github.com/unisabi)
+## License
+See `MIT LICENSE` for more information.
+## Deployment
 
-## Screenshot
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@ const apiKey = "31e9d74d1969cc655cd5d62c1919b31d";
 const topDiv = document.querySelector('.right-top')
 const bottomDiv = document.querySelector('.right-bottom')
 const cardDiv = document.getElementById('cardDiv');
-
+// creating a local storage for the cities
 let store = localStorage.cities ? JSON.parse(localStorage.cities) : [];
-
+// taking the infor from the cities and fetching the info
 const handleCities = city => {
   document.querySelector('input').value = city;
   getForecast();
@@ -23,11 +23,11 @@ handleStore();
 async function getForecast() {
   let city = document.querySelector('input').value;
   if (!city) return;
-  
+  // calling the api key we have
   let url =`https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&units=imperial&q=${city}`;
 
   let { list } = await fetch(url).then((response) => response.json());
-
+// pushing the local storage to save
   if(!store.includes(city)) {
     store.push(city);
     localStorage.cities = JSON.stringify(store);
@@ -35,7 +35,7 @@ async function getForecast() {
   };
 
   let { main:{temp, humidity}, dt, wind:{speed}, weather:[{icon}] } = list[0];
-
+// the right top div will show all the temp,wind and humidity and pulled the image source to pop up next to it
   topDiv.innerHTML = `
   <div>
     <h1>${city} (${new Date(dt*1000).toLocaleDateString()})</h1>
@@ -50,7 +50,7 @@ async function getForecast() {
 
   for (let i = 7; i < list.length; i+=8) {
     let { main:{temp, humidity}, dt, wind:{speed}, weather:[{icon}] } = list[i];
-
+    // again called the weather api for the card divs to show the temp wind and humidity
     cardDiv.innerHTML += `
     <div class="card">
       <h3>(${new Date(dt*1000).toLocaleDateString()})</h3> 
@@ -61,5 +61,5 @@ async function getForecast() {
     </div>`
   }
 };
-
+// we are always "listening" when someone clicks a button
 document.querySelector('button').addEventListener('click', getForecast);
